d57ae8018002aae7c9a02ff29a2bb8c586f251b4
kaitel <126915246+sakaitel@users.noreply.github.com>
2026-09-02 20:13:31 -0300

    reorganizando funcoes de config.sh

--- lib/config.sh
+++ lib/config.sh
@@ -3,17 +3,22 @@
 source "./lib/logger.sh"
 
 carregar_configuracao() {
-    local arquivo_config="cbuild.conf"
+    local arquivo_config="${1:-cbuild.conf}"
 
     # Valores padrão de reserva
-    NOME_EXECUTAVEL="arquivo_executavel"
+    NOME_EXECUTAVEL="TecProg"
     DIRETORIO_FONTE="src"
     NIVEL_OTIMIZACAO="-O2"
 
-    if [ -f "$arquivo_config" ]; then
-        source "$arquivo_config"
-        log_info "Configuracao carregada com sucesso de $arquivo_config."
-    else
-        log_info "Arquivo de configuracao nao encontrado. Usando padroes."
+    if [[ ! -f "$arquivo_config" ]]; then
+        log_error "O arquivo de configuracao '$arquivo_config' nao foi encontrado"
+        exit 1
+    elif [[ ! -r "$arquivo_config" ]]; then
+        log_error "O arquivo de configuracao '$arquivo_config' nao tem permissao de leitura"
+        exit 1
     fi
+
+    source "$arquivo_config"
+    log_info "Configuracao carregada com sucesso de $arquivo_config."
 }
+
