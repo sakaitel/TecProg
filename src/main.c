15c5fa4311c9a62b5c67597e10efccfcfd1c653f
sakaitel <sandrekaitel@gmail.com>
2026-09-14 16:10:15 -0300

    add utils.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "utils.h"
+
+void msg_teste(void) {
+    printf("Msg de teste da funcao msg_teste() em utils.h\n");
+}
 
 int main(){
     printf("Arquivo main.c carregado\n");
+    msg_teste();
     return 0;
 }
